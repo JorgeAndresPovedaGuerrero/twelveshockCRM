ead5a88bbcf73aa85dde75ae101ce98538ef2ce4
Andrés Poveda <andrespoveda.100@gmail.com>
2024-08-29 09:28:12 -0500

    Update dist with new build including base href change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importa este módulo
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { OrderFormComponent } from './components/order-form/order-form.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
 import { ApiService } from './services/api.service';
-import { MessageService } from './services/message.service'; // <--- Asegúrate de importar el servicio
+import { MessageService } from './services/message.service';
 import { HeaderComponent } from './header/header.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { FormsModule } from '@angular/forms';
 import { OrderEditModalComponent } from './order-edit-modal/order-edit-modal.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-
-@NgModule({ declarations: [
-        AppComponent,
-        OrderFormComponent,
-        OrderListComponent,
-        HeaderComponent,
-        OrderEditModalComponent,
-    ], // <--- Asegúrate de incluirlo en los providers
-    bootstrap: [AppComponent], imports: [NgbModule,
-        FormsModule,
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule, // Agrega este módulo
-        TooltipModule.forRoot(),
-        ToastrModule.forRoot()], providers: [ApiService, MessageService, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    OrderFormComponent,
+    OrderListComponent,
+    HeaderComponent,
+    OrderEditModalComponent,
+  ],
+  imports: [
+    NgbModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    ToastrModule.forRoot()
+  ],
+  providers: [
+    ApiService,
+    MessageService,
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // Añadido aquí
+  ],
+  bootstrap: [AppComponent]
+})
 export class AppModule { }
